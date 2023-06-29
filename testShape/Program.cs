1bee467e0045cbb49853d857927491be2217d41c
Mandurang <56937341+Mandurang@users.noreply.github.com>
2023-06-29 16:53:10 +0100

    Add project files.

---
+++ testShape/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+internal class Program
+{
+    [DllImport("DllShape.dll")]
+    public static extern void triangleType(double catetA, double catetB, double catetC);
+    private static void Main(string[] args)
+    {
+        double catetA = 3;
+        double catetB = 4;
+        double catetC = 5;
+
+        
+        triangleType(catetA, catetB, catetC);
+    }
+}
